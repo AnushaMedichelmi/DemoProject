69f229302e812d0c18eb5c298e1ee20ae74bed7b
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-29 14:37:01 +0530

    Namespace concept

---
+++ Assets/NameSpace.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace is a keyword where we use before namespace
+//syntax:namespace name {}
+public class NameSpace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NameSpaceDemo.MyNameClass.MyPrint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NameSpaceDemo.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+namespace NameSpaceDemo
+{
+    public class MyNameClass
+    {
+        public static void MyPrint()
+        {
+            Debug.Log("My Print Method from name sapce");
+        }
+    }
+}
